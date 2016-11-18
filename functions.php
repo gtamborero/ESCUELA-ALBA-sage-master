eb87f8eb18f606067954ed08cbd05a907c06891f
gtamborero <gtamborero@iproject.cat>
2016-11-18 16:42:14 +0100

    delete emoji + embed

--- functions.php
+++ functions.php
@@ -29,6 +29,14 @@ foreach ($sage_includes as $file) {
 unset($file, $filepath);
 
 
+// Eliminar emojis + wp-embed
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+function my_deregister_scripts(){
+  wp_deregister_script( 'wp-embed' );
+}
+add_action( 'wp_footer', 'my_deregister_scripts' );
+
 // EXCERPT LENGHT
 function custom_excerpt_length( $length ) {
 	return 20;
